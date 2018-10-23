72a138feb2ea6559104412822741ab1edbdd3a8e
Guillaume GIRARD <guillaume.girard@safeware.fr>
2018-10-23 17:11:47 +0200

    sentry conf

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -33,3 +33,17 @@ function environmentPath(env) {
 module.exports = function () {
   return useDefaultConfig;
 };
+
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
+
+module.exports = {
+  // other configuration
+  plugins: [
+    new SentryWebpackPlugin({
+      include: '.',
+      ignoreFile: '.sentrycliignore',
+      ignore: ['node_modules', 'webpack.config.js'],
+      configFile: 'sentry.properties'
+    })
+  ]
+};
