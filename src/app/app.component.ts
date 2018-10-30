5da6f3dcad1e5b06263d58e22f920a4f09bfb1c7
Guillaume GIRARD <guillaume.girard@safeware.fr>
2018-10-30 18:33:50 +0100

    end now button style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
+      statusBar.hide();
 
       // this language will be used as a fallback when a translation isn't found in the current language
       translate.addLangs(['en', 'fr']);
