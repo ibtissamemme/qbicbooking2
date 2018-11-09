601bac4c2bd9720db0ca70616e3c50af83c006e9
Guillaume GIRARD <guillaume.girard@safeware.fr>
2018-11-09 10:44:37 +0100

    correction to the env variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,35 +1,47 @@
 //r: 61, g:205, b: 88
 export const ENV = {
-  mode: 'Production',
-  endpoint: 'http://safeware-custk.hds-group.com/GesroomRestAPI/Gesroom/API',
-  apiKey: 'MEI97ZZ8POQFZZ2BIBWJPRNLSLPZ',
-  endpoint2: 'http://safeware-custk.hds-group.com/TelemaqueRestAPI',
-  apiKey2: 'MEI97ZZ8POQFZZ2BIBWJPRNLSLPZ',
-  adminId: 'SESA65737',
-  tabletId: '123456',
-  adminCode: '1607',
-  logo: 'logo_white.png',
+  mode: "Production",
+  endpoint: "http://safeware-custk.hds-group.com/GesroomRestAPI/Gesroom/API",
+  apiKey: "MEI97ZZ8POQFZZ2BIBWJPRNLSLPZ",
+  endpoint2: "http://safeware-custk.hds-group.com/HamiltonAppsAPI",
+  apiKey2: "MEI97ZZ8POQFZZ2BIBWJPRNLSLPZ",
+  adminId: "SESA65737",
+  tabletId: "123456",
+  adminCode: "1607",
+  logo: "logo_white.png",
   colors: {
     primary: {
-      r: 90, g:187, b: 61
+      r: 90,
+      g: 187,
+      b: 61
     },
     secondary: {
-      r:228, g: 127, b: 0
+      r: 228,
+      g: 127,
+      b: 0
     },
     danger: {
-      r:177, g: 0, b: 67
+      r: 177,
+      g: 0,
+      b: 67
     }
   },
   // because for some reason led colors do not look antyhing like colors on the screen...
   ledColors: {
     primary: {
-      r: 0, g:255, b: 0
+      r: 0,
+      g: 255,
+      b: 0
     },
     secondary: {
-      r:228, g: 127, b: 0
+      r: 228,
+      g: 127,
+      b: 0
     },
     danger: {
-      r:255, g: 0, b: 0
+      r: 255,
+      g: 0,
+      b: 0
     }
   }
 };
