60fc1ec623d1af2a75716997dd3aecee00eec058
Guillaume GIRARD <guillaume.girard@safeware.fr>
2019-12-16 17:31:15 +0100

    chore(build): update build for happs test portal

---
+++ src/environments/environment.myhapps.ts
@@ -0,0 +1,52 @@
+export const ENV = {
+  mode: "Production",
+  endpoint: "https://demo.myhamiltonapps.com/HamiltonAppsAPI",
+  apiKey: "MDJSMZZ4CTUPZZ2BIC05S145X6YP",
+  endpoint2: "https://demo.myhamiltonapps.com/HamiltonAppsAPI",
+  apiKey2: "MDJSMZZ4CTUPZZ2BIC05S145X6YP",
+  adminId: "taffin",
+  password: "2206",
+  tabletId: "123456",
+  adminCode: "1607",
+  logo: "logo_white.png",
+  defaultlang:"en",
+  prefix:"",
+  bookingStartHour:7,
+  bookingEndHour:20,
+  isPinInClearText:"true",
+  colors: {
+    primary: {
+      r: 90,
+      g: 187,
+      b: 61
+    },
+    secondary: {
+      r: 228,
+      g: 127,
+      b: 0
+    },
+    danger: {
+      r: 177,
+      g: 0,
+      b: 67
+    }
+  },
+  // because for some reason led colors do not look antyhing like colors on the screen...
+  ledColors: {
+    primary: {
+      r: 0,
+      g: 255,
+      b: 0
+    },
+    secondary: {
+      r: 228,
+      g: 100,
+      b: 0
+    },
+    danger: {
+      r: 255,
+      g: 0,
+      b: 0
+    }
+  }
+};
